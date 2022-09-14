63aa208eca9b7c439b4c6baed0de4fa9de636989
DESKTOP-SL0JOSE\tj-bu <>
2022-09-14 18:15:16 +0900

    포트폴리오를 그리드형태에서  텍스트가 메인으로 되는 형태로 변경 및 작성 2022-09-14 정형빈

---
+++ 나만의포트폴리오폴더/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/canvas.css">
+  <script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+  <script src="./js/script.js"></script>
+  <title>Document</title>
+</head>
+
+<body>
+    <main>
+      <section>
+        <ul>
+          <li><img src="./img/actionrabbit.jpg" ></li>
+          <li>portfolio</li>
+          <li>2022.09</li>
+        </ul>
+        <hr>
+        <ol>
+          <li>디스플레이 레벨</li> <br>
+          <li>마진과 패딩</li><br>
+          <li>포토샵</li><br>
+          <li>일러스트레이터</li><br>
+        </ol>
+      </section>
+    </main>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</body>
+
+</html>
