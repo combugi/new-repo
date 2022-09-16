12beadf144b406ebcb95e2a9e91830c86f55bdda
DESKTOP-SL0JOSE\tj-bu <>
2022-09-16 18:20:10 +0900

    포트폴리오 작업 2022-09-16 18:19 정형빈

--- 나만의포트폴리오폴더/index.php
+++ 나만의포트폴리오폴더/index.php
@@ -15,27 +15,53 @@
 </head>
 
 <body>
-    <main>
-      <section>
-        <ul>
-          <li><img src="./img/actionrabbit.jpg" ></li>
-          <li>portfolio</li>
-          <li>2022 <br>  09</li>
-        </ul>
-        <hr>
-        <ol>
-          <li><h3>HTML</h3><i class="fa-regular fa-rectangle-list"></i></li> <br>
-          <li><h3>SCSS</h3><i class="fa-regular fa-square"></i></li><br>
-          <li><h3>Js</h3><i class="fa-brands fa-js"></i></li><br>
-          <li><h3>PS</h3><i class="fa-solid fa-wand-magic-sparkles"></i></li><br>
-          <li><h3>IL</h3><i class="fa-solid fa-bezier-curve"></i></li><br>
-        </ol>
-      </section>
-    </main>
-  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-</body>
+  <main>
+    <section>
+      <ul class="introduce">
+        <li><img src="./img/actionrabbit.jpg"></li>
+        <li>
+          <dl>
+            <dt>name:</dt>
+            <dd>정형빈 </dd>
+            <dt>e-mail:</dt>
+            <dd>oooo@naver.com</dd>
+            <dt>mobile:</dt>
+            <dd>010-0000-0000</dd>
+            <dt>address:</dt>
+            <dd>부평시 부평구 부평리</dd>
+          </dl>
+        </li>
+      </ul>
+      <hr>
+      <ul>
+        <li>
+          <h3>HTML</h3><i class="fa-regular fa-rectangle-list"></i>
+        </li>
+        <li>
+          <h3>SCSS</h3><i class="fa-regular fa-square"></i>
+        </li>
+        <li>
+          <h3>Js</h3><i class="fa-brands fa-js"></i>
+        </li>
+        <li>
+          <h3>PS</h3><i class="fa-solid fa-wand-magic-sparkles"></i>
+        </li>
+        <li>
+          <h3>IL</h3><i class="fa-solid fa-bezier-curve"></i>
+        </li>
+      </ul>
+      <div class="mypassion">
+        <p>작가의 한 마디:본격적인 소개에 앞서 제가 이 일의 지원한 동기는 <strong>"성취" </strong> 라는 이 두단어 부터 시작되었습니다. <br>
+        주로 혼자서 작업하지만 때론 함께 만드는 그 과정으로부터 나온 결과가 모니터 너머로 보여줄수 있다는 점이 매우 인상적이였습니다.그래서 전 지원하고자 합니다. <br>
+          <q>We will find a way. We always have.
+            우리는 답을 찾을 것이다. 늘 그랬듯이</q>
+        </p>
+      </div>
+    </section>
 
+  
+  </main>
+
+</body>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </html>
